ea2b218e66e1761114477e899e26f5ba67f884b2
Wajiha11 <71877393+Wajiha11@users.noreply.github.com>
2021-12-23 11:25:29 +0000

    Update shallow2D.py

--- SWE/shallow2D.py
+++ SWE/shallow2D.py
@@ -18,7 +18,7 @@ Select the case:
     If I run the code after uncommenting the (EXACT SOLUTION) then case 1 and case 2 does not produce results. Still searching why.
     case 1 gives wrong results while case 2 gives results close to exact solution.
 '''
-case = 1
+case = 3
 
 ##  mesh ##
 n = 99
@@ -33,7 +33,7 @@ xslice = 0.5
 
 ######### PARAMETERS   ###############
 
-g = 1 # gravitational acceleration
+g = 9.8 # gravitational acceleration
 H = 1 # water depth
 t = 0
 m = 2
@@ -61,7 +61,7 @@ print('End time =', t_end)
 
 # dt = 0.005 # time step [s]  n/t_end
 dx= 1/n
-dt = 0.0018 # dx/(16*np.pi)
+dt = 0.001 # dx/(16*np.pi)
 print('time step size =', dt)
 
 ts = int(t_end/dt)
@@ -75,11 +75,21 @@ V = fd.FunctionSpace(mesh, "CG", 1)
 #### Define Function Spaces ####
 trial = fd.TrialFunction(V)
 print( 'The shape of trail funcion is= ',np.shape(trial))
+
 v = fd.TestFunction(V)
-phi = fd.Function(V, name = "phi") # phi^n
-phi_new = fd.Function(V, name = "phi_new") # phi^n+1
-eta = fd.Function(V, name =  "eta") # eta^n
-eta_new = fd.Function(V, name =  "eta_new") # eta^n+1
+v2 = fd.TestFunction(V)
+
+phi1 = fd.Function(V, name = "phi1") # phi^n
+phi1_new = fd.Function(V, name = "phi1_new") # phi^n+1
+eta1 = fd.Function(V, name =  "eta1") # eta^n
+eta1_new = fd.Function(V, name =  "eta1_new") # eta^n+1
+
+
+phi2 = fd.Function(V, name = "phi2") # phi^n
+phi2_new = fd.Function(V, name = "phi2_new") # phi^n+1
+eta2 = fd.Function(V, name =  "eta2") # eta^n
+eta2_new = fd.Function(V, name =  "eta2_new") # eta^n+1
+
 
 phie= fd.Function(V, name = "phi_exact") 
 etae = fd.Function(V, name = "eta_exact") 
@@ -88,36 +98,26 @@ etae = fd.Function(V, name = "eta_exact")
 A = 1
 B = 1
 
+phi = fd.Function(V, name = "phi") # phi^n
+eta = fd.Function(V, name =  "eta") # eta^n
+
 
 ic1 = phi.interpolate( fd.cos(k1 * x) * fd.cos(k2 * y) * ( -A*fd.sin(w*t) + B*fd.cos(w*t) ) * (g/w) )
 ic2 = eta.interpolate( fd.cos(k1 * x) * fd.cos(k2 * y) * ( A*fd.cos(w*t) + B*fd.sin(w*t) ) )
 
 
-phi.assign(ic1)
-phi_new.assign(ic1)
+
+phi1.assign(ic1)
+phi1_new.assign(ic1)
+eta1.assign(ic2)
+eta1_new.assign(ic2)
+
+phi2.assign(ic1)
+phi2_new.assign(ic1)
+eta2.assign(ic2)
+eta2_new.assign(ic2)
 
 
-eta.assign(ic2)
-eta_new.assign(ic2)
-
-######## FIGURE SETTINGS ###########
-
-# plt.figure(1)
-fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2)
-# fig, (ax1, ax2) = plt.subplots(1, 2)
-ax1.set_title(r'$\eta$ value in $x$ direction')
-ax2.set_title(r'$\phi$ value in $x$ direction')
-ax3.set_title(r'$\eta$ value in $y$ direction')
-ax4.set_title(r'$\phi$ value in $y$ direction')
-
-ax1.set_xlabel(r'$x$ ')
-ax1.set_ylabel(r'$\eta$ ')
-ax2.set_xlabel(r'$x$ ')
-ax2.set_ylabel(r'$\phi$ ')
-ax3.set_xlabel(r'$y$ ')
-ax3.set_ylabel(r'$\eta$ ')
-ax4.set_xlabel(r'$y$ ')
-ax4.set_ylabel(r'$\phi$ ')
 
 ###### EXACT SOLUTION #########
 
@@ -141,10 +141,10 @@ phievals = np.array([phi_exact.at(x, yslice) for x in xvals])
 etaevalsy = np.array([eta_exact.at(xslice, y) for y in yvals])
 phievalsy =  np.array([phi_exact.at(xslice, y) for y in yvals])
 
-ax1.plot(xvals, etaevals, '--',label = '$Exact: \eta_x$ ')
-ax2.plot(xvals, phievals, '--',label = '$Exact: \phi_x$ ')
-ax3.plot(yvals, etaevalsy, '--',label = '$Exact: \eta_y$ ')
-ax4.plot(yvals, phievalsy, '--',label = '$Exact: \phi_y$ ')
+# ax1.plot(xvals, etaevals, '--',label = '$Exact: \eta_x$ ')
+# ax2.plot(xvals, phievals, '--',label = '$Exact: \phi_x$ ')
+# ax3.plot(yvals, etaevalsy, '--',label = '$Exact: \eta_y$ ')
+# ax4.plot(yvals, phievalsy, '--',label = '$Exact: \phi_y$ ')
 
 # #ax1.legend(loc='upper right')
 # #ax2.legend(loc='upper left')
@@ -154,44 +154,87 @@ ax4.plot(yvals, phievalsy, '--',label = '$Exact: \phi_y$ ')
 if case == 1:
     print("You have selected case 1 : VP solved by firedrake to compute the eqs of motion ")
 
-    VP = ( fd.inner ((eta_new - eta)/dt , phi) - fd.inner(phi_new , (eta_new/dt)) - (1/2 * H * fd.inner(fd.grad(phi), fd.grad(phi))) - (1/2 * g * fd.inner(eta_new,eta_new)) ) * fd.dx
+    VP = ( fd.inner ((eta1_new - eta1)/dt , phi1) - fd.inner(phi1_new , (eta1_new/dt)) - (1/2 * H * fd.inner(fd.grad(phi1), fd.grad(phi1))) - (1/2 * g * fd.inner(eta1_new,eta1_new)) ) * fd.dx
     
-    eta_expr = fd.derivative(VP, phi, v)  # derivative of VP wrt phi^n to get the expression for eta^n+1
-    print('eta_expression=',eta_expr)
-    eta_expr = fd.NonlinearVariationalSolver(fd.NonlinearVariationalProblem(eta_expr, eta_new))
-    phi_expr = fd.derivative(VP, eta_new, v)  # derivative of VP wrt eta^n+1 to get the value of phi^n+1
-    print('phi_expression=',phi_expr)
-    phi_expr = fd.NonlinearVariationalSolver(fd.NonlinearVariationalProblem(phi_expr, phi_new))
+    eta1_expr = fd.derivative(VP, phi1, v)  # derivative of VP wrt phi^n to get the expression for eta^n+1
+    print('eta_expression=',eta1_expr)
+    eta1_expr = fd.NonlinearVariationalSolver(fd.NonlinearVariationalProblem(eta1_expr, eta1_new))
+    phi1_expr = fd.derivative(VP, eta1_new, v)  # derivative of VP wrt eta^n+1 to get the value of phi^n+1
+    print('phi_expression=',phi1_expr)
+    phi1_expr = fd.NonlinearVariationalSolver(fd.NonlinearVariationalProblem(phi1_expr, phi1_new))
     
     ###### OUTPUT FILES ##########
-    outfile_phi = fd.File("results_case1/phi.pvd")
-    outfile_eta = fd.File("results_case1/eta.pvd")
+    outfile_phi1 = fd.File("results_case1/phi1.pvd")
+    outfile_eta1 = fd.File("results_case1/eta1.pvd")
     
     
     ######### TIME LOOP ############
     while (t <= t_end):
-        eta_expr.solve()
+        eta1_expr.solve()
         t+= dt
-        phi_expr.solve()
+        phi1_expr.solve()
         
-        outfile_eta.write( eta_new )
-        outfile_phi.write( phi_new )
+        outfile_eta1.write( eta1_new )
+        outfile_phi1.write( phi1_new )
         
         # set-up next time-step
-        phi.assign(phi_new)
-        eta.assign(eta_new)
+        phi1.assign(phi1_new)
+        eta1.assign(eta1_new)
 
 
-    eta1vals = np.array([eta_new.at(x, yslice) for x in xvals])
-    phi1vals = np.array([phi_new.at(x, yslice) for x in xvals])
+    eta1vals = np.array([eta1_new.at(x, yslice) for x in xvals])
+    phi1vals = np.array([phi1_new.at(x, yslice) for x in xvals])
     
-    eta1valsy = np.array([eta_new.at(xslice, y) for y in yvals])
-    phi1valsy =  np.array([phi_new.at(xslice, y) for y in yvals])
+    eta1valsy = np.array([eta1_new.at(xslice, y) for y in yvals])
+    phi1valsy =  np.array([phi1_new.at(xslice, y) for y in yvals])
     
-    ax1.plot(xvals, eta1vals, label = 'Case1 : $\eta_x$')
-    ax2.plot(xvals,phi1vals, label = 'Case1 : $\phi_x$')
-    ax3.plot(yvals, eta1valsy,label = 'Case1 : $\eta_y$')
-    ax4.plot(yvals,phi1valsy, label = 'Case1 : $\phi_y$')
+    error_e1x = etaevals - eta1vals
+    error_p1x = phievals - phi1vals
+    error_e1y = etaevalsy - eta1valsy
+    error_p1y =  phievalsy - phi1valsy 
+    
+    fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2)
+    ax1.set_title(r'$\eta$ value in $x$ direction')
+    ax2.set_title(r'$\phi$ value in $x$ direction')
+    ax3.set_title(r'$\eta$ value in $y$ direction')
+    ax4.set_title(r'$\phi$ value in $y$ direction')
+    
+    ax1.set_xlabel(r'$x$ ')
+    ax1.set_ylabel(r'$\eta$ ')
+    ax2.set_xlabel(r'$x$ ')
+    ax2.set_ylabel(r'$\phi$ ')
+    ax3.set_xlabel(r'$y$ ')
+    ax3.set_ylabel(r'$\eta$ ')
+    ax4.set_xlabel(r'$y$ ')
+    ax4.set_ylabel(r'$\phi$ ')
+    
+    ax1.plot(xvals, eta1vals, label = 'Case 1 : $\eta_x$')
+    ax2.plot(xvals,phi1vals, label = 'Case 1 : $\phi_x$')
+    ax3.plot(yvals, eta1valsy,label = 'Case 1 : $\eta_y$')
+    ax4.plot(yvals,phi1valsy, label = 'Case 1 : $\phi_y$')
+    
+    ax1.plot(xvals, etaevals, '--',label = '$Exact: \eta_x$ ')
+    ax2.plot(xvals, phievals, '--',label = '$Exact: \phi_x$ ')
+    ax3.plot(yvals, etaevalsy, '--',label = '$Exact: \eta_y$ ')
+    ax4.plot(yvals, phievalsy, '--',label = '$Exact: \phi_y$ ')
+    
+    
+    ax1.legend(loc=2)
+    ax2.legend(loc=1)
+    ax3.legend(loc=2)
+    ax4.legend(loc=1)
+    
+    fig1, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2)
+
+    ax1.set_title(r'$L_{\infty}$-norm for $\eta$ in $x$ direction')
+    ax2.set_title(r'$L_{\infty}$-norm for $\phi$ in $x$ direction')
+    ax3.set_title(r'$L_{\infty}$-norm for $\eta$ in $y$ direction')
+    ax4.set_title(r'$L_{\infty}-norm for $\phi$ in $y$ direction')
+    
+    ax1.plot(xvals, error_e1x, label = 'Case1 : $\eta_x$')
+    ax2.plot(xvals, error_p1x, label = 'Case1 : $\phi_x$')
+    ax3.plot(yvals, error_e1y, label = 'Case1 : $\eta_y$')
+    ax4.plot(yvals, error_p1y, label = 'Case1 : $\phi_y$')
     
     
     ax1.legend(loc=2)
@@ -203,46 +246,246 @@ if case == 1:
 elif case == 2:
       
     print('You have selected case 2: First calculates eta^n+1 and then phi^(n+1) like the given problem')
-    eta2_full = (v * (eta_new - eta)/dt - H * fd.inner(fd.grad(v), fd.grad(phi)))* fd.dx
-    eta2_full = fd.NonlinearVariationalSolver(fd.NonlinearVariationalProblem(eta2_full, eta_new))
+    eta2_full = (v * (eta2_new - eta2)/dt - H * fd.inner(fd.grad(v), fd.grad(phi2)))* fd.dx
+    eta2_full = fd.NonlinearVariationalSolver(fd.NonlinearVariationalProblem(eta2_full, eta2_new))
     
-    phi2_full = (v* (phi_new - phi)/dt  + g*v*eta_new) * fd.dx
-    phi2_full = fd.NonlinearVariationalSolver(fd.NonlinearVariationalProblem(phi2_full, phi_new))
+    phi2_full = (v* (phi2_new - phi2)/dt  + g*v*eta2_new) * fd.dx
+    phi2_full = fd.NonlinearVariationalSolver(fd.NonlinearVariationalProblem(phi2_full, phi2_new))
     
     
     ###### OUTPUT FILES ##########
-    outfile_phi = fd.File("results_case2/phi.pvd") # case 4 in shallow2.py
-    outfile_eta = fd.File("results_case2/eta.pvd")
+    outfile_phi2 = fd.File("results_case2/phi.pvd") # case 4 in shallow2.py
+    outfile_eta2 = fd.File("results_case2/eta.pvd")
     
     while t<= t_end:
         eta2_full.solve()
         t+= dt
         phi2_full.solve()
-        outfile_eta.write( eta_new )
-        outfile_phi.write( phi_new )
+        outfile_eta2.write( eta2_new )
+        outfile_phi2.write( phi2_new )
         
         # set-up next time-step
-        phi.assign(phi_new)
-        eta.assign(eta_new)
+        phi2.assign(phi2_new)
+        eta2.assign(eta2_new)
         
-    eta2vals = np.array([eta_new.at(x, yslice) for x in xvals])
-    phi2vals = np.array([phi_new.at(x, yslice) for x in xvals])
+    eta2vals = np.array([eta2_new.at(x, yslice) for x in xvals])
+    phi2vals = np.array([phi2_new.at(x, yslice) for x in xvals])
     
-    eta2valsy = np.array([eta_new.at(xslice, y) for y in yvals])
-    phi2valsy =  np.array([phi_new.at(xslice, y) for y in yvals])
+    eta2valsy = np.array([eta2_new.at(xslice, y) for y in yvals])
+    phi2valsy =  np.array([phi2_new.at(xslice, y) for y in yvals])
     # print('phi_case2 =', phi3vals)
     # print('eta_case2 =', eta3vals)
-    ax1.plot(xvals, eta2vals, label = 'Case2 : $\eta_x$')
-    ax2.plot(xvals,phi2vals, label = 'Case2 : $\phi_x$')
-    ax3.plot(yvals, eta2valsy,label = 'Case2 : $\eta_y$')
-    ax4.plot(yvals,phi2valsy, label = 'Case2 : $\phi_y$')
+    
+    error_e2x = etaevals - eta2vals
+    error_p2x = phievals - phi2vals  
+    error_e2y = etaevalsy - eta2valsy
+    error_p2y =  phievalsy - phi2valsy
+    
+    fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2)
+    ax1.set_title(r'$\eta$ value in $x$ direction')
+    ax2.set_title(r'$\phi$ value in $x$ direction')
+    ax3.set_title(r'$\eta$ value in $y$ direction')
+    ax4.set_title(r'$\phi$ value in $y$ direction')
+    
+    ax1.set_xlabel(r'$x$ ')
+    ax1.set_ylabel(r'$\eta$ ')
+    ax2.set_xlabel(r'$x$ ')
+    ax2.set_ylabel(r'$\phi$ ')
+    ax3.set_xlabel(r'$y$ ')
+    ax3.set_ylabel(r'$\eta$ ')
+    ax4.set_xlabel(r'$y$ ')
+    ax4.set_ylabel(r'$\phi$ ')
+    
+    ax1.plot(xvals, eta2vals, label = 'Case 2 : $\eta_x$')
+    ax2.plot(xvals,phi2vals, label = 'Case 2 : $\phi_x$')
+    ax3.plot(yvals, eta2valsy,label = 'Case 2  : $\eta_y$')
+    ax4.plot(yvals,phi2valsy, label = 'Case 2 : $\phi_y$')
+    
+    ax1.plot(xvals, etaevals, '--',label = '$Exact: \eta_x$ ')
+    ax2.plot(xvals, phievals, '--',label = '$Exact: \phi_x$ ')
+    ax3.plot(yvals, etaevalsy, '--',label = '$Exact: \eta_y$ ')
+    ax4.plot(yvals, phievalsy, '--',label = '$Exact: \phi_y$ ')
     
     
     ax1.legend(loc=2)
     ax2.legend(loc=1)
     ax3.legend(loc=2)
     ax4.legend(loc=1)
+    
+    
+    fig1, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2)
 
+    ax1.set_title(r'$L_{\infty}$-norm for $\eta$ in $x$ direction')
+    ax2.set_title(r'$L_{\infty}$-norm for $\phi$ in $x$ direction')
+    ax3.set_title(r'$L_{\infty}$-norm for $\eta$ in $y$ direction')
+    ax4.set_title(r'$L_{\infty}-norm for $\phi$ in $y$ direction')
+    
+    # ax1.plot(xvals, error_e1x, label = 'Case1 : $\eta_x$')
+    # ax2.plot(xvals, error_p1x, label = 'Case1 : $\phi_x$')
+    # ax3.plot(yvals, error_e1y, label = 'Case1 : $\eta_y$')
+    # ax4.plot(yvals, error_p1y, label = 'Case1 : $\phi_y$')
+    
+    ax1.plot(xvals, error_e2x, label = 'Case 2 : $\eta_x$')
+    ax2.plot(xvals, error_p2x, label = 'Case 2 : $\phi_x$')
+    ax3.plot(yvals, error_e2y, label = 'Case 2 : $\eta_y$')
+    ax4.plot(yvals, error_p2y, label = 'Case 2 : $\phi_y$')
+    
+    ax1.legend(loc=2)
+    ax2.legend(loc=1)
+    ax3.legend(loc=2)
+    ax4.legend(loc=1)
+    
+    
+
+elif case == 3:
+    
+     print("Case 1 and Case 2 will be solved and compared with the exact solution")
+     print('Case 2 has been initialised')
+    
+     eta2_full = (v2 * (eta2_new - eta2)/dt - H * fd.inner(fd.grad(v2), fd.grad(phi2)))* fd.dx
+     eta2_full = fd.NonlinearVariationalSolver(fd.NonlinearVariationalProblem(eta2_full, eta2_new))
+    
+     phi2_full = (v2 * (phi2_new - phi2)/dt  + g*v2*eta2_new) * fd.dx
+     phi2_full = fd.NonlinearVariationalSolver(fd.NonlinearVariationalProblem(phi2_full, phi2_new))
+    
+    
+     ###### OUTPUT FILES ##########
+     outfile_phi2 = fd.File("results_case2/phi2.pvd") # case 4 in shallow2.py
+     outfile_eta2 = fd.File("results_case2/eta2.pvd")
+    
+     while t<= t_end:
+        eta2_full.solve()
+        t+= dt
+        phi2_full.solve()
+        outfile_eta2.write( eta2_new )
+        outfile_phi2.write( phi2_new )
+        
+        # set-up next time-step
+        phi2.assign(phi2_new)
+        eta2.assign(eta2_new)
+    
+    
+        
+     eta2vals = np.array([eta2_new.at(x, yslice) for x in xvals])
+     phi2vals = np.array([phi2_new.at(x, yslice) for x in xvals])
+    
+     eta2valsy = np.array([eta2_new.at(xslice, y) for y in yvals])
+     phi2valsy =  np.array([phi2_new.at(xslice, y) for y in yvals])
+   
+     error_e2x = etaevals - eta2vals
+     error_p2x = phievals - phi2vals  
+     error_e2y = etaevalsy - eta2valsy
+     error_p2y =  phievalsy - phi2valsy
+    
+     print('Case 2 has been completed')
+    
+   
+
+     print("Case 1 has been initialized")     
+    
+     VP = ( fd.inner ((eta1_new - eta1)/dt , phi1) - fd.inner(phi1_new , (eta1_new/dt)) - (1/2 * H * fd.inner(fd.grad(phi1), fd.grad(phi1))) - (1/2 * g * fd.inner(eta1_new,eta1_new)) ) * fd.dx
+    
+     eta1_expr = fd.derivative(VP, phi1, v)  # derivative of VP wrt phi^n to get the expression for eta^n+1
+     print('eta_expression=',eta1_expr)
+     eta1_expr = fd.NonlinearVariationalSolver(fd.NonlinearVariationalProblem(eta1_expr, eta1_new))
+     phi1_expr = fd.derivative(VP, eta1_new, v)  # derivative of VP wrt eta^n+1 to get the value of phi^n+1
+     print('phi_expression=',phi1_expr)
+     phi1_expr = fd.NonlinearVariationalSolver(fd.NonlinearVariationalProblem(phi1_expr, phi1_new))
+    
+    ###### OUTPUT FILES ##########
+     outfile_phi1 = fd.File("results_case1/phi1.pvd")
+     outfile_eta1 = fd.File("results_case1/eta1.pvd")
+    
+    
+    ######### TIME LOOP ############
+     t = 0
+     while (t <= t_end):
+        eta1_expr.solve()
+        t+= dt
+        phi1_expr.solve()
+        
+        outfile_eta1.write( eta1_new )
+        outfile_phi1.write( phi1_new )
+        
+        # set-up next time-step
+        phi1.assign(phi1_new)
+        eta1.assign(eta1_new)
+
+    
+     eta1vals = np.array([eta1_new.at(x, yslice) for x in xvals])
+     phi1vals = np.array([phi1_new.at(x, yslice) for x in xvals])
+    
+     eta1valsy = np.array([eta1_new.at(xslice, y) for y in yvals])
+     phi1valsy =  np.array([phi1_new.at(xslice, y) for y in yvals])
+    
+     error_e1x = etaevals - eta1vals
+     error_p1x = phievals - phi1vals
+     error_e1y = etaevalsy - eta1valsy
+     error_p1y =  phievalsy - phi1valsy
+    
+     print("Case 1 has been completed") 
+
+    # print('phi_case2 =', phi3vals)
+    # print('eta_case2 =', eta3vals)
+    
+     fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2)
+    # fig, (ax1, ax2) = plt.subplots(1, 2)
+     ax1.set_title(r'$\eta$ value in $x$ direction')
+     ax2.set_title(r'$\phi$ value in $x$ direction')
+     ax3.set_title(r'$\eta$ value in $y$ direction')
+     ax4.set_title(r'$\phi$ value in $y$ direction')
+    
+     ax1.set_xlabel(r'$x$ ')
+     ax1.set_ylabel(r'$\eta$ ')
+     ax2.set_xlabel(r'$x$ ')
+     ax2.set_ylabel(r'$\phi$ ')
+     ax3.set_xlabel(r'$y$ ')
+     ax3.set_ylabel(r'$\eta$ ')
+     ax4.set_xlabel(r'$y$ ')
+     ax4.set_ylabel(r'$\phi$ ')
+    
+     ax1.plot(xvals, eta1vals, 'c-', label = 'Case 1 : $\eta_x$')
+     ax2.plot(xvals,phi1vals, 'c-', label = 'Case 1 : $\phi_x$')
+     ax3.plot(yvals, eta1valsy, 'c-',label = 'Case 1 : $\eta_y$')
+     ax4.plot(yvals,phi1valsy, 'c-', label = 'Case 1 : $\phi_y$')
+    
+     ax1.plot(xvals, eta2vals, 'r:', label = 'Case 2 : $\eta_x$')
+     ax2.plot(xvals,phi2vals, 'r:',label = 'Case 2 : $\phi_x$')
+     ax3.plot(yvals, eta2valsy,'r:',label = 'Case 2  : $\eta_y$')
+     ax4.plot(yvals,phi2valsy, 'r:',label = 'Case 2 : $\phi_y$')
+    
+     ax1.plot(xvals, etaevals, '--',label = '$Exact: \eta_x$ ')
+     ax2.plot(xvals, phievals, '--',label = '$Exact: \phi_x$ ')
+     ax3.plot(yvals, etaevalsy, '--',label = '$Exact: \eta_y$ ')
+     ax4.plot(yvals, phievalsy, '--',label = '$Exact: \phi_y$ ')
+    
+    
+     ax1.legend(loc=2)
+     ax2.legend(loc=1)
+     ax3.legend(loc=2)
+     ax4.legend(loc=1)
+    
+     fig1, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2)
+
+     ax1.set_title(r'$L_{\infty}$-norm for $\eta$ in $x$ direction')
+     ax2.set_title(r'$L_{\infty}$-norm for $\phi$ in $x$ direction')
+     ax3.set_title(r'$L_{\infty}$-norm for $\eta$ in $y$ direction')
+     ax4.set_title(r'$L_{\infty}-norm for $\phi$ in $y$ direction')
+    
+     ax1.plot(xvals, error_e1x, 'c-',label = 'Case1 : $\eta_x$')
+     ax2.plot(xvals, error_p1x, 'c-',label = 'Case1 : $\phi_x$')
+     ax3.plot(yvals, error_e1y, 'c-',label = 'Case1 : $\eta_y$')
+     ax4.plot(yvals, error_p1y, 'c-',label = 'Case1 : $\phi_y$')
+    
+     ax1.plot(xvals, error_e2x, 'r:', label = 'Case 2 : $\eta_x$')
+     ax2.plot(xvals, error_p2x, 'r:', label = 'Case 2 : $\phi_x$')
+     ax3.plot(yvals, error_e2y, 'r:', label = 'Case 2 : $\eta_y$')
+     ax4.plot(yvals, error_p2y, 'r:', label = 'Case 2 : $\phi_y$')
+    
+     ax1.legend(loc=2)
+     ax2.legend(loc=1)
+     ax3.legend(loc=2)
+     ax4.legend(loc=1)
     
     
 plt.show()     
